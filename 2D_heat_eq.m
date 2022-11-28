03c8d3112fa0a5054b2e792c120b38b6e0565ef9
Seth Minor <97004318+SethMinor@users.noreply.github.com>
2022-11-27 18:52:09 -0800

    Update 2D_heat_eq.m

--- 2D_heat_eq.m
+++ 2D_heat_eq.m
@@ -1,20 +1,25 @@
-% COOK THE 2D TURKEY
+%% COOK THE 2D TURKEY
 clear, clc, clf;
+
+% Font size, for plotting
+fs = 14;
 
 % 2D heat equation, with forcing
 % PDE: u_t - (u_xx + u_yy) = f(x,y) (with Dirichlet BC's)
 
-% WEAK FORM (assuming no flux)
-% int(u_t*v)dV + int(grad(u)*grad(v))dV = int(f*v)dV
+% WEAK FORM
+% int(u_t*v)dV + int(grad(u)*grad(v))dV + int(BC's)dV = int(f*v)dV
+% Insert u = sum(u_i(t)v_i(x,y)) and f = sum(f_i(t)v_i(x,y))
 % Then discretize u_t
 
+% STEADY STATE: KU=F
+% DYNAMICS: MU'+KU=F
 
-% Nice examples?:
-% https://www.mathworks.com/matlabcentral/fileexchange/77545-2d-heat-transfer-fem
+%https://www.wias-berlin.de/people/peschka/teaching/fem_present.pdf
 
 
 
-% Define the rectangular domain
+%% Define the rectangular domain
 %               Lx
 %      ----------------------
 %      |                    |
@@ -33,86 +38,42 @@ dy = Ly/(Ny-1); % y-axis discretization
 
 
 
-% Discretize 2D rectangular domain into triangular elements
-%      ----------------------       NODE NUMBERING
-%      |/\/\/\/\/\/\/\/\/\/\|       1    -> 2    -> ... -> Nx
-%      |/\/\/\/\/\/\/\/\/\/\|       Nx+1 -> Nx+2 -> ... -> 2*Nx
-%      |/\/\/\/\/\/\/\/\/\/\|                  ...
-%      ----------------------       ... ->  ...  -> ... -> Nx*Ny
+%% Discretize 2D rectangular domain into triangular elements
+% And choose a basis of tent functions for interior nodes
 
-% Triangular elements are defined by three boundary nodes, e.g.:
-%     1 -- 2                 2
-%     |  /                  /|
-%     | /         or       / |      etc.
-%     |/                  /  |
-%   (Nx+1)           (Nx+1)--(Nx+2)
+myelement =@(x,y,a,b) max(1 - abs(x-a) - abs(y-b),0);
 
-nodes = zeros(Ny,Nx); % nodes for discretized domain
+[x,y] = meshgrid(1:15,1:15);
+T = delaunay(x,y);
+% [x,y] = meshgrid(0:dx:Lx,0:dy:Ly);
+% T = delaunay(x,y);
 
-% Define the Cartesian coords of the nodes, e.g.:
-% (x,y)=(0dx,0dy)       (x,y)=(1dx,0dy)             (x,y)=(Lx,0dy)
-%     (1) ----------------- (2) ----- ...  ------------ (Nx)
-% (x,y)=(0dx,1dy)       (x,y)=(1dx,1dy)             (x,y)=(Lx,1dy)
-%    (Nx+1) ------------- (Nx+2) ----- ...  ---------- (2*Nx)
-%                           ...
-% (x,y)=(0dx,Ly)        (x,y)=(1dx,Ly)              (x,y)=(Lx,Ly)
-%    ([Nx-1]*Ny) ------ ([Nx-1]*Ny + 1) --- ...  ----- (Nx*Ny)
+z1 = myelement(x,y,10,10);
+z2 = myelement(x,y,5,5);
+% z1 = myelement(x,y,3*dx,4*dy);
+% z2 = myelement(x,y,1*dx,6*dy);
 
-x = 0:dx:Lx;
-y = 0:dy:Ly;
-[X,Y] = meshgrid(x,y); % Cartesian coordinates at 'nodes'
+figure (1)
+trisurf(T,x,y,z1,'facecolor',[0 0.4470 0.7410])
+alpha 0.75; hold on;
+trisurf(T,x,y,z2,'facecolor',[0.8500 0.3250 0.0980])
+alpha 0.75; hold on;
+trisurf(T,x,y,0*z2,'facecolor','w')
+alpha 0.75;
+axis equal;
+colormap jet;
 
-% Define the Cartesian coords of the vertices for each element
-% (Listed in element order, 2*(Nx-1)*(Ny-1) total elements)
-% vertices = [[X(1,1),Y(1,1)]; [X(1,2),Y(1,1)]; [X(1,1),Y(2,1)]];
-% figure (1)
-% plot([vertices(:,1);vertices(1,1)],[vertices(:,2);vertices(1,2)])
-% grid on
-
-% LOOP OVER EACH PAIR OF X'S AND PICK A Y BELOW?!
-
-% Initialize (3 vertices) x (2*(Nx-1)*(Ny-1) element) array
-num_elements = 2*(Nx-1)*(Ny-1);
-vertices = zeros(num_elements,3);
-% for i = 1:Nx
-%     for j = 1:Ny
-%         disp([X(j,i),Y(j,i)])
-% %         % Even-numbered elements face one way
-% %         if (mod(j,2) == 0)
-% %             %vertices(j,:) = [[X(1,1),Y(1,1)]; [X(1,2),Y(1,1)]; [X(1,1),Y(2,1)]];
-% %     
-% %         % Odd-numbered elements face another way
-% %         elseif (mod(i,2) == 1)
-% %             %ertices(j,:) = [[0, 0]; [0, 0]; [0, 0]];
-% %         end
-%     end
-% end
-
-counter = 0;
-
-for i = 1:(Nx-1)
-
-    %disp(i)
-    counter = counter + 1;
-    fprintf('Vertex %.0f (up)\n',counter)
-    %disp([[X(1,i),Y(1,i)];[X(1,i+1),Y(1,i+1)];[X(2,i),Y(2,i)]])
-    fprintf('(%.2f,%.2f), (%.2f,%.2f), (%.2f,%.2f)\n\n',X(1,i),Y(1,i),X(1,i+1),Y(1,i+1),X(2,i),Y(2,i))
-    
-    counter = counter + 1;
-    fprintf('Vertex %.0f (down)\n',counter)
-    %disp([[X(1,i),Y(1,i)];[X(1,i+1),Y(1,i+1)];[X(2,i+1),Y(2,i+1)]])
-    fprintf('(%.2f,%.2f), (%.2f,%.2f), (%.2f,%.2f)\n\n',X(2,i),Y(2,i),X(1,i+1),Y(1,i+1),X(2,i+1),Y(2,i+1))
-end
+title('Triangulation of domain','Interpreter','latex','FontSize',fs)
+xlabel('$x$','Interpreter','latex','FontSize',fs)
+ylabel('$y$','Interpreter','latex','FontSize',fs)
+zlabel('$z$','Interpreter','latex','FontSize',fs)
+legend('Example shape function','Example shape function',...
+    'Interpreter','latex','FontSize',fs-4)
 
 
 
 
-%DT = delaunay(x,y);
-%triplot(DT,x,y);
-
-
-
-% Dirichlet boundary conditions
+%% Dirichlet boundary conditions
 %               B2
 %      ----------------------
 %      |                    |
@@ -122,9 +83,9 @@ end
 %               B3
 
 B1 = 0;
-B2 = 3;
+B2 = 0;
 B3 = 0;
-B4 = 1;
+B4 = 0;
 
 % Initial condition (t = 0)
 
@@ -137,3 +98,72 @@ Nt = ceil((tf-t0)/dt);
 % Stability conidition is apparently dt <= 1/(2*((1/dx)^2+(1/dy)^2))
 
 %Turk = questdlg('Cook the turkey?','God calling:','Oh yeah','Nay','Nay');
+
+
+
+% DUMPATRON
+% %      ----------------------       NODE NUMBERING
+% %      |/\/\/\/\/\/\/\/\/\/\|       1    -> 2    -> ... -> Nx
+% %      |/\/\/\/\/\/\/\/\/\/\|       Nx+1 -> Nx+2 -> ... -> 2*Nx
+% %      |/\/\/\/\/\/\/\/\/\/\|                  ...
+% %      ----------------------       ... ->  ...  -> ... -> Nx*Ny
+% 
+% % Triangular elements are defined by three boundary nodes, e.g.:
+% %     1 -- 2                 2
+% %     |  /                  /|
+% %     | /         or       / |      etc.
+% %     |/                  /  |
+% %   (Nx+1)           (Nx+1)--(Nx+2)
+% 
+% nodes = zeros(Ny,Nx); % nodes for discretized domain
+% 
+% % Define the Cartesian coords of the nodes, e.g.:
+% % (x,y)=(0dx,0dy)       (x,y)=(1dx,0dy)             (x,y)=(Lx,0dy)
+% %     (1) ----------------- (2) ----- ...  ------------ (Nx)
+% % (x,y)=(0dx,1dy)       (x,y)=(1dx,1dy)             (x,y)=(Lx,1dy)
+% %    (Nx+1) ------------- (Nx+2) ----- ...  ---------- (2*Nx)
+% %                           ...
+% % (x,y)=(0dx,Ly)        (x,y)=(1dx,Ly)              (x,y)=(Lx,Ly)
+% %    ([Nx-1]*Ny) ------ ([Nx-1]*Ny + 1) --- ...  ----- (Nx*Ny)
+% 
+% x = 0:dx:Lx;
+% y = 0:dy:Ly;
+% [X,Y] = meshgrid(x,y); % Cartesian coordinates at 'nodes'
+% 
+% % Define the Cartesian coords of the vertices for each element
+% % (Listed in element order, 2*(Nx-1)*(Ny-1) total elements)
+% 
+% % Initialize (3 vertices) x (2*(Nx-1)*(Ny-1) element) array
+% num_elements = 2*(Nx-1)*(Ny-1);
+% vertices = zeros(num_elements,6);
+% 
+% % F
+% counter = 0;
+% for i = 1:(Nx-1)
+%     for j = 1:(Ny-1)
+%         counter = counter + 1;
+%         fprintf('Vertex %.0f (up)\n',counter)
+%         fprintf('(%.2f,%.2f), (%.2f,%.2f), (%.2f,%.2f)\n\n',...
+%             X(j,i),Y(j,i),X(j,i+1),Y(j,i+1),X(j+1,i),Y(j+1,i))
+%         vertices(counter,:) = [X(j,i),Y(j,i),X(j,i+1),Y(j,i+1),X(j+1,i),Y(j+1,i)];
+%         
+%         counter = counter + 1;
+%         fprintf('Vertex %.0f (down)\n',counter)
+%         fprintf('(%.2f,%.2f), (%.2f,%.2f), (%.2f,%.2f)\n\n',...
+%             X(j+1,i),Y(j+1,i),X(j,i+1),Y(j,i+1),X(j+1,i+1),Y(j+1,i+1))
+%         vertices(counter,:) = [X(j+1,i),Y(j+1,i),X(j,i+1),Y(j,i+1),X(j+1,i+1),Y(j+1,i+1)];
+%     end
+% end
+% 
+% figure (1)
+% for j = 1:num_elements
+%     plot([vertices(j,1);vertices(j,3);vertices(j,5);vertices(j,1)],...
+%     [vertices(j,2);vertices(j,4);vertices(j,6);vertices(j,2)],'-k')
+%     hold on
+% end
+% grid on
+% xlim([-0.1,Lx+0.1])
+% ylim([-0.1,Ly+0.1])
+% title('Triangulation of domain','Interpreter','latex','FontSize',fs)
+% xlabel('$x$','Interpreter','latex','FontSize',fs)
+% ylabel('$y$','Interpreter','latex','FontSize',fs)

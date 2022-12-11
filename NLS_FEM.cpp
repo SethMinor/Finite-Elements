36560b4ed9a4cd2d27702ae62afaa526cec76f85
Seth Minor <97004318+SethMinor@users.noreply.github.com>
2022-12-10 20:06:03 -0800

    Update NLS_FEM.cpp

--- NLS_FEM.cpp
+++ NLS_FEM.cpp
@@ -22,10 +22,10 @@ macro grad(u) [dx(u), dy(u)]//EOM
 // Discretization and end-time
 // (Note dx is pre-defined)
 verbosity = 0;
-real Dx = .01, dt = 0.001, idt = 1./dt, T = 0.5;
+real Dx = .01, dt = 0.0005, idt = 1./dt, T = 0.5;
 
 // Sign of non-linearity
-real sigma = -1.;
+real sigma = 1.;
 
 // Build the border of the mesh
 // Parametric borders must have continuous arrows
@@ -38,26 +38,24 @@ border right(t = 0, 1){x = 2*pi; y = 2*pi*t; label = 2;};
 func perio = [[4,y],[2,y],[3,x],[1,x]];
 
 // Discretize the domain
-// Adpative mesh refinement (number = constant mesh size, nbvx = max number of vertices)
-// Can adapt the mesh in a loop over time, based on value of u:
-// https://doc.freefem.org/documentation/mesh-generation.html#the-command-adaptmesh
 mesh Th = buildmesh(bottom(1/Dx) + top(1/Dx) + left(1/Dx) + right(1/Dx), fixedborder = true);
-real mymeshsize = 0.05;
+real mymeshsize = 0.01;
 Th = adaptmesh(Th, mymeshsize, IsMetric = 1, nbvx = 100000, periodic = perio); // periodic square
 plot(Th, wait = true, cmm = "Refined mesh");
 
 // Initialize finite element space type
-fespace Vh(Th, P2, periodic = perio);
+fespace Vh(Th, P1, periodic = perio);
 Vh<complex> u, v;
 
 // Define mu
 real mu = 20;
 
 // Define IC
-//func u0 = -exp(-(x - pi)^2-(y - pi)^2-5); // Gaussian
-//func u0 = sqrt(mu)*tanh(sqrt(mu)*sqrt((x - pi)^2 + (y - pi)^2)); // one vortex
-func u0 = sqrt(mu)*tanh(sqrt(mu)*sqrt((x - 3*pi/2)^2 + (y - 3*pi/2)^2)) *
-					tanh(sqrt(mu)*sqrt((x - pi/2)^2 + (y - pi/2)^2)); // two vortices
+//func u0 = exp(-(x - pi)^2-(y - pi)^2 - 5); // Gaussian
+//func u0 = tanh(x)+1i;
+func u0 = sqrt(mu)*tanh(sqrt(mu)*sqrt((x - pi)^2 + (y - pi)^2)); // one vortex
+//func u0 = sqrt(mu)*tanh(sqrt(mu)*sqrt((x - 3*pi/2)^2 + (y - 3*pi/2)^2)) *
+//					tanh(sqrt(mu)*sqrt((x - pi/2)^2 + (y - pi/2)^2)); // two vortices
 //func u0 = sqrt(mu); // flat background
 cout << "Initial mass is " << int2d(Th)((u0*conj(u0))^2) << endl;
 
@@ -65,8 +63,8 @@ cout << "Initial mass is " << int2d(Th)((u0*conj(u0))^2) << endl;
 Vh<complex> uold = u0, nonlin = u0*conj(u0);
 
 // Adaptive mesh refinement based on IC
-Vh fh = abs(u0);
-plot(fh, wait = true, cmm = "Initial condition", fill = true, dim = 3);
+Vh fh = abs(u0), fhplot = fh^2;
+plot(fhplot, wait = true, cmm = "Initial condition", fill = true, dim = 3);
 Th = adaptmesh(Th, fh, periodic = perio); // periodic square
 plot(Th, wait = true, cmm = "Adaptive mesh refinement");
 
@@ -83,7 +81,7 @@ while (t <= T){
 	nonlin = u*conj(u);
 	uold = u;
 	plot(nonlin, dim = 3,
-		cmm = "t = " + t + "  ; \nMass = " + int2d(Th)(nonlin), fill = 1);
+		cmm = "Mass = " + int2d(Th)(nonlin) + ", t = " + t, fill = 1);
 
 	// Adaptive mesh refinement?
 	Vh fh = abs(u);
